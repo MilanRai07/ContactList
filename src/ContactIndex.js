6d8d1a137e186d9529f626fd2ac4fdd6987b0ac5
milanrai <milanraiyt07@gmail.com>
2023-12-08 22:29:55 +0545

    Add contact feature is added

--- src/ContactIndex.js
+++ src/ContactIndex.js
@@ -1,28 +1,74 @@
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import './css/Contact.css';
 import List from './List';
 import { ReactComponent as Search } from './assets/search.svg';
+import { ReactComponent as Plus } from "./assets/plus.svg";
 import AddContact from './AddContact';
-const ContactIndex=()=>{
-   const [show,setShow]=useState(false);
- return(
-  <>
+
+const parentData = createContext();                      //creation for context ApI
+
+const ContactIndex = () => {
+  const [show, setShow] = useState(true);                   // show list or addcontact component
+  const [defaultImg, setDefaultImg] = useState(true);     //show default add image icon or uploadedpicture
+  const [image, setImage] = useState();                   //store image uploaded 
+  const [person, setPerson] = useState();                 //for name
+  const [number, setNumber] = useState();                //for phone number
+  const [list, setList] = useState([]);                //store datas
+
+
+  const handleChange = (event) => {                    
+    setImage(URL.createObjectURL(event.target.files[0]));
+    setDefaultImg(false);
+  }
+
+  const addCircle = () => {                      //circel button/icon to redirect to add contact component
+    setShow(false);
+  }
+  const add = () => {                           //store datas into list
+    let id=new Date().toLocaleTimeString();
+    setList((olditems) => {
+      return [...olditems, {id:id, image: image, person: person, number: number }]
+    })
+    setShow(true);
+    setPerson("");
+    setNumber("");
+    setDefaultImg(true);
+  }
+  const contextValue = {                //values to be send to child components
+    imgDefault: defaultImg,
+    image: image,
+    person: person,
+    number: number,
+    setPerson: setPerson,
+    setNumber: setNumber,
+    add: add,
+    list: list,
+    handleChange: handleChange
+
+  }
+  return (
+    <>
       <div className='contact-container'>
         <div className='contact'>
-        <div className='contact-head'>
-          <h2>
-               Contacts
-          </h2>  
-              <Search/>
-          </div>
-          { show?
-          <List/>
-          :
-          <AddContact/>
-          }        
-      </div>  
+          <parentData.Provider value={contextValue}>             {/* provider tag*/}
+            <div className='contact-head'>
+              <h2>
+                Contacts
+              </h2>
+              <Search />
+            </div>
+            {show ?
+              <List />
+              :
+              <AddContact />
+            }
+            <Plus className='contact-adder' onClick={addCircle} />
+          </parentData.Provider>
+        </div>
+
       </div>
-  </>
- )
+    </>
+  )
 }
 export default ContactIndex;
+export { parentData };
--- src/List.js
+++ src/List.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import './css/list.css';
+import { parentData } from "./ContactIndex";
 const List = () => {
-    let img = require('./assets/person1.jpg');
+    const context = useContext(parentData);
+    const { list } = context;
     return (
         <>
-            <div className="contact-list">
-                <div className="detail">
-                    <div className="img-container">
-                    <img src={img} className="image" />
+            {list.map((ele, index) => {
+                return (
+                    <div className="contact-list" key={index}>
+                        <div className="detail">
+                            <div className="img-container">
+                                <img src={ele.image} className="image" />
+                            </div>
+                            <div className="detail-info">
+                                <h3>{ele.person}</h3>
+                                <p className="number">{ele.number}</p>
+                            </div>
+                        </div>
+                        <div className="list-event">
+                            <button className="edit-buton">Edit</button>
+                            <button className="del-buton" >Delete</button>
+                        </div>
                     </div>
-                    <div className="detail-info">
-                        <h3>Milan Rai</h3>
-                        <p className="number">9860797531</p>
-                    </div>
-                </div>
-                <div className="list-event">
-                    <button className="edit-buton">Edit</button>
-                    <button className="del-buton" >Delete</button>
-                </div>
-            </div>
+                )
+            })
+
+            }
         </>
     )
 }
