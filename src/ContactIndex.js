97909fff532e30978a3db9cbb90f6fd01b715e19
milanrai <milanraiyt07@gmail.com>
2023-12-09 11:15:52 +0545

    updated add feature

--- src/ContactIndex.js
+++ src/ContactIndex.js
@@ -2,37 +2,53 @@ import { createContext, useState } from 'react';
 import './css/Contact.css';
 import List from './List';
 import { ReactComponent as Search } from './assets/search.svg';
-import { ReactComponent as Plus } from "./assets/plus.svg";
+import { ReactComponent as Plus } from "./assets/pluss.svg";
+import { ReactComponent as Cancel } from "./assets/cancel.svg";
 import AddContact from './AddContact';
 
 const parentData = createContext();                      //creation for context ApI
 
 const ContactIndex = () => {
   const [show, setShow] = useState(true);                   // show list or addcontact component
-  const [defaultImg, setDefaultImg] = useState(true);     //show default add image icon or uploadedpicture
+  const [back,setBack]=useState(true) ;                      //for canceling of add contact to back home
+  const [defaultImg, setDefaultImg] = useState(true);      //show default add image icon or uploadedpicture
   const [image, setImage] = useState();                   //store image uploaded 
   const [person, setPerson] = useState();                 //for name
   const [number, setNumber] = useState();                //for phone number
   const [list, setList] = useState([]);                //store datas
 
 
-  const handleChange = (event) => {                    
+  const handleChange = (event) => {
     setImage(URL.createObjectURL(event.target.files[0]));
     setDefaultImg(false);
   }
 
   const addCircle = () => {                      //circel button/icon to redirect to add contact component
     setShow(false);
+    setBack(false);
   }
-  const add = () => {                           //store datas into list
-    let id=new Date().toLocaleTimeString();
-    setList((olditems) => {
-      return [...olditems, {id:id, image: image, person: person, number: number }]
-    })
+  const backCircle=()=>{                      //to cancel the add option 
     setShow(true);
-    setPerson("");
+    setBack(true);
+    setImage("");
     setNumber("");
-    setDefaultImg(true);
+    setPerson("");
+  }
+  const add = () => {                           //store datas into list 
+    let id = new Date().toLocaleTimeString();
+
+    if ( person && number !== null) {
+      setList((olditems) => {
+        return [...olditems, { id: id, image: image, person: person, number: number }]
+      })
+
+      setShow(true);
+      setPerson("");
+      setNumber("");
+      setDefaultImg(true);
+    } else {
+      console.log("name is required");
+    }
   }
   const contextValue = {                //values to be send to child components
     imgDefault: defaultImg,
@@ -62,7 +78,11 @@ const ContactIndex = () => {
               :
               <AddContact />
             }
+            {back?
             <Plus className='contact-adder' onClick={addCircle} />
+            :
+            <Cancel className='contact-back' onClick={backCircle}/>
+            }
           </parentData.Provider>
         </div>
 
